f980f778fd565245598e8a12f304da67566396e7
Clark Richards <clark.richards@gmail.com>
2025-09-23 13:53:10 -0300

    update to change blink-cmp-copilot to just blink-copilot

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,107 +1,107 @@
 return {
-    "saghen/blink.cmp",
-    opts = {
-        snippets = {
-            expand = function(snippet, _)
-                return LazyVim.cmp.expand(snippet)
-            end,
-        },
-        appearance = {
-            -- sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release, assuming themes add support
-            use_nvim_cmp_as_default = false,
-            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- adjusts spacing to ensure icons are aligned
-            nerd_font_variant = "mono",
-        },
-        completion = {
-            accept = {
-                -- experimental auto-brackets support
-                auto_brackets = {
-                    enabled = true,
-                },
-            },
-            menu = {
-                draw = {
-                    treesitter = { "lsp" },
-                },
-            },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 200,
-            },
-            ghost_text = {
-                enabled = vim.g.ai_cmp,
-            },
-        },
+	"saghen/blink.cmp",
+	opts = {
+		snippets = {
+			expand = function(snippet, _)
+				return LazyVim.cmp.expand(snippet)
+			end,
+		},
+		appearance = {
+			-- sets the fallback highlight groups to nvim-cmp's highlight groups
+			-- useful for when your theme doesn't support blink.cmp
+			-- will be removed in a future release, assuming themes add support
+			use_nvim_cmp_as_default = false,
+			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+		completion = {
+			accept = {
+				-- experimental auto-brackets support
+				auto_brackets = {
+					enabled = true,
+				},
+			},
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+			},
+			ghost_text = {
+				enabled = vim.g.ai_cmp,
+			},
+		},
 
-        -- experimental signature help support
-        -- signature = { enabled = true },
+		-- experimental signature help support
+		-- signature = { enabled = true },
 
-        sources = {
-            -- adding any nvim-cmp sources here will enable them
-            -- with blink.compat
-            compat = {},
-            default = { "lsp", "path", "snippets", "buffer", "copilot" },
-            -- default = { "lsp", "path", "snippets", "buffer" },
-            providers = {
-                lsp = {
-                    name = "lsp",
-                    enabled = true,
-                    module = "blink.cmp.sources.lsp",
-                    kind = "LSP",
-                    -- When linking markdown notes, I would get snippets and text in the
-                    -- suggestions, I want those to show only if there are no LSP
-                    -- suggestions
-                    --
-                    -- Enabled fallbacks as this seems to be working now
-                    -- Disabling fallbacks as my snippets wouldn't show up when editing
-                    -- lua files
-                    -- fallbacks = { "snippets", "buffer" },
-                    score_offset = 100, -- the higher the number, the higher the priority
-                },
-                path = {
-                    name = "Path",
-                    module = "blink.cmp.sources.path",
-                    score_offset = 50,
-                    -- When typing a path, I would get snippets and text in the
-                    -- suggestions, I want those to show only if there are no path
-                    -- suggestions
-                    fallbacks = { "snippets", "buffer" },
-                    opts = {
-                        trailing_slash = false,
-                        label_trailing_slash = true,
-                        get_cwd = function(context)
-                            return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-                        end,
-                        show_hidden_files_by_default = true,
-                    },
-                },
-                buffer = {
-                    name = "Buffer",
-                    enabled = true,
-                    max_items = 3,
-                    module = "blink.cmp.sources.buffer",
-                    min_keyword_length = 4,
-                    score_offset = 25, -- the higher the number, the higher the priority
-                },
-                copilot = {
-                    name = "copilot",
-                    enabled = true,
-                    module = "blink-cmp-copilot",
-                    kind = "Copilot",
-                    min_keyword_length = 3,
-                    score_offset = 25, -- the higher the number, the higher the priority
-                    async = true,
-                },
-            },
-            -- cmdline = {},
-        },
+		sources = {
+			-- adding any nvim-cmp sources here will enable them
+			-- with blink.compat
+			compat = {},
+			default = { "lsp", "path", "snippets", "buffer", "copilot" },
+			-- default = { "lsp", "path", "snippets", "buffer" },
+			providers = {
+				lsp = {
+					name = "lsp",
+					enabled = true,
+					module = "blink.cmp.sources.lsp",
+					kind = "LSP",
+					-- When linking markdown notes, I would get snippets and text in the
+					-- suggestions, I want those to show only if there are no LSP
+					-- suggestions
+					--
+					-- Enabled fallbacks as this seems to be working now
+					-- Disabling fallbacks as my snippets wouldn't show up when editing
+					-- lua files
+					-- fallbacks = { "snippets", "buffer" },
+					score_offset = 100, -- the higher the number, the higher the priority
+				},
+				path = {
+					name = "Path",
+					module = "blink.cmp.sources.path",
+					score_offset = 50,
+					-- When typing a path, I would get snippets and text in the
+					-- suggestions, I want those to show only if there are no path
+					-- suggestions
+					fallbacks = { "snippets", "buffer" },
+					opts = {
+						trailing_slash = false,
+						label_trailing_slash = true,
+						get_cwd = function(context)
+							return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+						end,
+						show_hidden_files_by_default = true,
+					},
+				},
+				buffer = {
+					name = "Buffer",
+					enabled = true,
+					max_items = 3,
+					module = "blink.cmp.sources.buffer",
+					min_keyword_length = 4,
+					score_offset = 25, -- the higher the number, the higher the priority
+				},
+				copilot = {
+					name = "copilot",
+					enabled = true,
+					module = "blink-copilot",
+					kind = "Copilot",
+					min_keyword_length = 3,
+					score_offset = 25, -- the higher the number, the higher the priority
+					async = true,
+				},
+			},
+			-- cmdline = {},
+		},
 
-        keymap = {
-            preset = "default",
-            ["<Tab>"] = { "select_and_accept" },
-        },
-    },
+		keymap = {
+			preset = "default",
+			["<Tab>"] = { "select_and_accept" },
+		},
+	},
 }
