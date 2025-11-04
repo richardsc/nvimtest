2be0eff2cef128642a3855aa1897444d1d09d5f2
Clark Richards <clark.richards@gmail.com>
2025-11-04 13:19:09 -0400

    add julia/iron keymaps from Dan

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,8 +4,40 @@
 
 -- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "r", "rmd", "rnw" },
-  callback = function()
-    vim.b.autoformat = false
-  end,
+    pattern = { "r", "rmd", "rnw" },
+    callback = function()
+        vim.b.autoformat = false
+    end,
+})
+
+-- Make CR send one line to the Julia console
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "julia",
+    callback = function()
+        local iron = require("iron.core")
+        -- Start the Julia REPL with <leader>jr
+        vim.keymap.set("n", "<leader>jr", "<cmd>IronRepl<CR>", {
+            buffer = true,
+            desc = "Start Julia REPL",
+        })
+        -- Send current line to Julia REPL, then move down one line
+        vim.keymap.set("n", "<CR>", function()
+            iron.send_line()
+            vim.cmd("normal! j")
+            desc = "Send current line to Julia REPL"
+        end, {
+            buffer = true,
+            desc = "Send line to Julia REPL and move down",
+        })
+        -- Visual mode: send selected lines and stay on next line after selection
+        vim.keymap.set("v", "<CR>", function()
+            iron.visual_send()
+            -- move cursor to the line after the visual selection
+            vim.cmd("normal! gv")  -- reselect the last visual selection
+            vim.cmd("normal! `>j") -- jump to end and go down one line
+        end, {
+            buffer = true,
+            desc = "Send visual selection to Julia REPL and move down",
+        })
+    end,
 })
