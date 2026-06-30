6a99b17cc7718696b9ac83e119ac503acf15ff49
Clark Richards <clark.richards@gmail.com>
2026-06-30 16:52:49 -0300

    lensline updates

--- lua/plugins/lensline.lua
+++ lua/plugins/lensline.lua
@@ -1,8 +1,9 @@
 return {
-	"oribarilan/lensline.nvim",
-	tag = "2.0.0", -- or: branch = 'release/2.x' for latest non-breaking updates
-	event = "LspAttach",
-	config = function()
-		require("lensline").setup()
-	end,
+    "oribarilan/lensline.nvim",
+    -- tag = "2.0.0", -- or: branch = 'release/2.x' for latest non-breaking updates
+    branch = "release/2.x",
+    event = "LspAttach",
+    config = function()
+        require("lensline").setup()
+    end,
 }
