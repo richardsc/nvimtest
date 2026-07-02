5581924d4118f7cb5afb821753a86d1617122bbf
Clark Richards <clark.richards@gmail.com>
2026-07-02 14:42:31 -0300

    make sure to use italics highlight even if the colorscheme doesn't use it by default

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- Force comments and keywords to use Victor Mono's cursive italic style
+vim.api.nvim_set_hl(0, "Comment", { italic = true })
+vim.api.nvim_set_hl(0, "Keyword", { italic = true })
